2bf6f239177778629160a4c52daa72f7e8321fd4
wph1234 <Aa1663110891821@Outlook.com>
2025-06-22 16:40:38 +0800

    急修改:11.11

--- GTMBplugin_B/Parts/customcmds/customcmdsPart.py
+++ GTMBplugin_B/Parts/customcmds/customcmdsPart.py
@@ -17,7 +17,7 @@ compExtra = CFServer.CreateExtraData(levelId)
 compBlockEntity = CFServer.CreateBlockEntity(levelId)
 
 serversystem = serverApi.GetSystem('Minecraft', 'preset')
-copyrightInfo = "§b---------\n版本： v0.8a(2025/6):11.1\n© 2025 联机大厅服务器模板\n本项目采用 GNU General Public License v3.0 许可证。\n---------"
+copyrightInfo = "§b---------\n版本： v0.8a(2025/6):11.11\n© 2025 联机大厅服务器模板\n本项目采用 GNU General Public License v3.0 许可证。\n---------"
 
 def create_players_str(players):
 	#type: (list) -> str
@@ -1508,7 +1508,7 @@ class customcmdsPart(PartBase):
 					removeMotion = CompMotion.RemovePlayerMotion
 				else:
 					removeMotion = CompMotion.RemoveEntityMotion
-				for ii in list(Motions):
+				for ii in Motions[:]:
 					removeMotion(ii)
 					Motions.remove(ii)
 				compExtra.SetExtraData('Motions', Motions)
